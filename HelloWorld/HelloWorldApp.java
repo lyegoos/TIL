fd09afff706f16b0331a50136cd62240aa011d46
lyegoos <94948834+lyegoos@users.noreply.github.com>
2022-08-31 23:44:46 +0900

    Add files via upload
    22.08.31 egoingJAVA TIL

---
+++ HelloWorld/HelloWorldApp.java
@@ -0,0 +1,5 @@
+public class HelloWorldApp{	// 클래스명과 파일명이 일치해야한다
+	public static void main(String[] args) {	// 이름이 일치하는 클래스의 main 메서드를 호출
+		System.out.println("Hello World!!");	// 콘솔에 Hello World!! 를 출력
+	}
+}
